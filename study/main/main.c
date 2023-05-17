209ee8e9c738967e97c43873d8904cbff271d41c
Tech Rick <45835951+richu101@users.noreply.github.com>
2023-05-17 23:05:07 +0530

    intrrept code added'

--- study/main/main.c
+++ study/main/main.c
@@ -5,28 +5,60 @@
 #include "esp_log.h"
 #include  "driver/gpio.h"
 
-#include "arduino_fun.h"
 
-#define PIR 26
-#define LED 25
+#define switchpin 26
 
+QueueHandle_t intreptqueue;
+
+static void IRAM_ATTR gpio_isr_handler(void *args)
+{
+    int pin = (int *)args;
+    xQueueSendFromISR(intreptqueue,&pin,NULL); 
+}
+void motiondetected(void *args)
+{ 
+    int PinNumber;
+    int pin ,count = 0;
+    while(1)
+    {
+        
+        if(xQueueReceive(intreptqueue,&pin,portMAX_DELAY))
+        {
+            PinNumber = pin;
+            // disable the intrrept for removing douncing
+            gpio_isr_handler_remove(switchpin);
+            // wait for 10ms
+
+            //do work
+            do
+            {
+                vTaskDelay(20/portTICK_PERIOD_MS);
+            } while (gpio_get_level(switchpin)==1);
+            
+
+            printf("Pin %d is HIGH",PinNumber);
+
+            // Enable intrrept
+            gpio_isr_handler_add(switchpin,gpio_isr_handler,(void *) switchpin);
+
+        }
+        printf("motion detected - Count = %d\n",count++);
+        
+    }
+}
 void app_main(void)
 {
 
-pinMode(PIR,input_pulldown);
-pinMode(LED,output);
-while(1)
-{
+gpio_pad_select_gpio(switchpin);
+gpio_set_direction(switchpin, GPIO_MODE_INPUT);
+gpio_pulldown_en(switchpin);
+gpio_pullup_dis(switchpin);
+gpio_set_intr_type(switchpin,GPIO_INTR_POSEDGE);
 
-int level = gpio_get_level(PIR);
-if(level)
-{
-gpio_set_level(LED,(uint32_t) level);
-delay(5000);
-}
-gpio_set_level(LED,(uint32_t) level);
-delay(1);
+intreptqueue = xQueueCreate(10,sizeof(int));
+xTaskCreate(motiondetected," motion detected",2040,NULL,1,NULL);
 
-}
+gpio_install_isr_service(0);
+gpio_isr_handler_add(switchpin,gpio_isr_handler,(void *) switchpin);
 
 }
